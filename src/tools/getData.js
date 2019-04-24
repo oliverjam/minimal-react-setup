563bb0ac05d6da2ebd24de98394c81f549b689eb
mrfong <therealshadix@gmail.com>
2019-04-24 14:59:56 +0300

    added a fetch for github api

--- src/tools/getData.js
+++ src/tools/getData.js
@@ -1,3 +1,19 @@
 // import { accessToken } from "../../token";
 export const accessToken = "6953b440ab762a5d9de0112d3be3f6a179533e0e";
 export const API_URL = "https://api.github.com";
+
+const checkResponse = response => {
+  if (response.status !== 200) {
+    return(`Error with the request! ${response.status}`);
+
+  }
+  return response.json();
+};
+
+export const getData = url => {
+  return fetch(`${url}?access_token=${accessToken}`)
+    .then(checkResponse)
+    .catch(err => {
+      throw new Error(`fetch getUserData failed ${err}`);
+    });
+};
